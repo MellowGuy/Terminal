00a2a3be4ab5ee3a3549682bd440a435f8486ad6
MellowGuy <thotyl88@gmail.com>
2017-11-07 14:13:56 -0500

    Added Hacker.cs file
    following along with the tutorial.

---
+++ Terminal Hacker Project/Assets/WM2000/Hacker.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+	public string greeting = "Hello Dave...";
+
+	// Use this for initialization
+	void Start()
+	{
+		ShowMainMenu();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void ShowMainMenu()
+	{
+		Terminal.ClearScreen();
+
+		Terminal.WriteLine(greeting);
+		Terminal.WriteLine("What would you like to hack into?\n");
+
+		Terminal.WriteLine("Press 1 for the local library.");
+		Terminal.WriteLine("Press 2 for the police station.");
+		Terminal.WriteLine("Press 3 for NASA.\n");
+
+		Terminal.WriteLine("Enter your selection:");
+	}
+}
